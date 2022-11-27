aaa81a8b07343a658f2f8d90645aad3684646651
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2022-11-27 22:05:58 +0100

    Add files via upload

---
+++ Statičke varijable/Statičke varijable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staticke_varijable
+{
+    class PrvaKlasa
+    {
+        private static int counter = 0;
+
+        public int Counter { get => counter; }
+
+        static PrvaKlasa()
+        {
+            counter++;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrvaKlasa x = new PrvaKlasa();
+            PrvaKlasa y = new PrvaKlasa();
+            PrvaKlasa z = new PrvaKlasa();
+
+            Console.WriteLine(x.Counter);
+            Console.WriteLine(y.Counter);
+            Console.WriteLine(z.Counter);
+
+            Console.ReadKey();
+        }
+    }
+}
